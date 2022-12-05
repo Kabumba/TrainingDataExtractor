9dd9290a5d1562194029cb28e4a577fb5394b2fe
Kabumba <Freddy253@live.de>
2022-12-05 15:37:17 +0100

    umordnen

--- umordnen.py
+++ umordnen.py
@@ -8,15 +8,17 @@ import torch
 import IO_manager
 from IO_manager import Directories
 
+
 dirs = Directories()
 state_dir = dirs.BASE_DIR + "/Train"
 trash_dir = dirs.BASE_DIR + "/Unused"
 gs = os.listdir(state_dir)
-zero = torch.zeros(101)
 upsi_files = []
 for f in gs:
     base_name = os.path.splitext(os.path.basename(f))[0]
     if f.startswith("[GaussSteps"):
+        shutil.move(os.path.join(state_dir, f), os.path.join(trash_dir, f))
+    if f.startswith("[ConstantSplit,0."):
         shutil.move(os.path.join(state_dir, f), os.path.join(trash_dir, f))
     if f.startswith("[Interpolator"):
         shutil.move(os.path.join(state_dir, f), os.path.join(trash_dir, f))
